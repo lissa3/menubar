9bf32c5b47f0fef2ba2b7e00f5973bf7de6a6ee0
lissa3 <tspan2017@gmail.com>
2020-01-11 18:08:47 +0100

    dynamic nav menu

---
+++ scripts.js
@@ -0,0 +1,19 @@
+// jquery-touchswipe
+
+var overlay = $(".overlay");
+var mainMenu = $(".main-nav");
+$(".menu-trigger").on("click",function(){
+    // only > than slim jq
+    // should match with transition time .main-nav
+    // overlay.fadeIn(400);
+    $(".overlay").show();
+    mainMenu.addClass("active");
+})
+// chaining:
+// events(click on .overlay == .main-nav-item
+// same effect = overlay div disappears)
+// .hide explicit ovelay
+$(".overlay,.main-nav-item").on("click",function(){    
+    overlay.hide();    
+    mainMenu.removeClass("active");
+})
